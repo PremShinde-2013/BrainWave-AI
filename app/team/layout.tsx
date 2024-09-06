dfa191cff767b7875055daee9fe4cd6dceaf875e
Prem Prakash Shinde <shindeprem102@gmail.com>
2024-09-06 22:13:44 +0530

    started with UI of home page

---
+++ app/team/layout.tsx
@@ -0,0 +1,15 @@
+
+
+export default function AboutLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10 lg:mt-60 '>
+			<div className='  text-center justify-center  w-screen lg:mt-60 mt-[1600px]'>
+				{children}
+			</div>
+		</section>
+	);
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,9 @@
 import type { Config } from "tailwindcss";
+const svgToDataUri = require("mini-svg-data-uri");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 
 const config: Config = {
     darkMode: ["class"],
@@ -8,7 +13,61 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
+  	extend: {  animation: {
+        "spin-around": "spin-around calc(var(--speed) * 2) infinite linear",
+			slide: "slide var(--speed) ease-in-out infinite alternate",
+		        scroll:
+				"scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+				  marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+ 
+      
+      },
+		  keyframes: {
+			  "shine-pulse": {
+				  "0%": {
+					  "background-position": "0% 0%",
+				  },
+				  "50%": {
+					  "background-position": "100% 100%",
+				  },
+				  to: {
+					  "background-position": "0% 0%",
+				  },
+			  },
+			   marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
+		    scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+        "spin-around": {
+          "0%": {
+            transform: "translateZ(0) rotate(0)",
+          },
+          "15%, 35%": {
+            transform: "translateZ(0) rotate(90deg)",
+          },
+          "65%, 85%": {
+            transform: "translateZ(0) rotate(270deg)",
+          },
+          "100%": {
+            transform: "translateZ(0) rotate(360deg)",
+          },
+        },
+        slide: {
+          to: {
+            transform: "translate(calc(100cqw - 100%), 0)",
+          },
+        },
+      },
   		backgroundImage: {
   			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
   			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
@@ -62,6 +121,32 @@ const config: Config = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+	plugins: [require("tailwindcss-animate"),
+	  addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          "bg-dot-thick": (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
+      );
+    },
+  ],
 };
 export default config;
+
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
