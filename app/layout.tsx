99a1379add0a80f3483bf41792b9ec4137d4dac8
Prem Prakash Shinde <shindeprem102@gmail.com>
2024-09-08 17:16:11 +0530

    added favicon and description of website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,33 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "BrainWave-AI",
-  description: "BrainWave-AI",
+  title: "Brainwave.ai - Your personal second brain.",
+  description:
+    "Bring saved information from all over the internet into one place where you can connect it, and ask AI about it",
+  metadataBase: {
+    host: "https://brainwave-ai.pages.dev",
+    href: "/",
+    origin: "https://brainwave-ai.pages.dev",
+    password: "brainwave",
+    hash: "brainwave",
+    pathname: "/",
+    search: "",
+    username: "brainwave",
+    hostname: "brainwave.ai",
+    port: "",
+    protocol: "https:",
+    searchParams: new URLSearchParams(""),
+    toString: () => "https://brainwave-ai.pages.dev/",
+    toJSON: () => "https://brainwave-ai.pages.dev/",
+  },
+  manifest: "/manifest.webmanifest",
+
 };
 
 export default function RootLayout({
@@ -18,14 +37,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Favicon links */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+      </head>
       <body className={inter.className}>
-         <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
         >{children}
-      </ThemeProvider></body>
+        </ThemeProvider></body>
     </html>
   );
 }
